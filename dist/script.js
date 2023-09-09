7564caf194dcf5ae250c90c9273b2ad7b0e1e50e
Guenfoudi-Mohamed <mohamed.guenfoudi@hotmail.com>
2023-09-09 17:47:30 +0100

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,154 @@
+"use strict";
+// start ***app To Do List***
+// === set pattern for input task 
+const inpTask = document.querySelector('body .container .form .head .inps input.inpTask');
+inpTask.addEventListener('keyup', () => {
+    const regEx = /^[a-zA-Z0-9]+\s*\W*/;
+    inpTask.value = inpTask.value.trimLeft();
+    const inpTestValue = regEx.test(inpTask.value);
+    if (inpTestValue === false) {
+        inpTask.classList.remove('valid');
+        inpTask.classList.add('invalid');
+    }
+    else if (inpTestValue) {
+        inpTask.classList.remove('invalid');
+        inpTask.classList.add('valid');
+    }
+});
+// === event for button add Task
+const btnAddTask = document.querySelector('body .container .form .head .inps input.btnAdd');
+btnAddTask.onclick = function () {
+    if (inpTask.classList.contains('valid')) {
+        const obj = new Task(inpTask.value);
+        inpTask.value = ``;
+        inpTask.focus();
+        inpTask.classList.remove('valid', 'invalid');
+    }
+    else {
+        inpTask.classList.add('invalid');
+        inpTask.value = ``;
+        inpTask.focus();
+    }
+};
+// === class Task
+class Task {
+    constructor(valueTask) {
+        this.id = Task.idTask(valueTask);
+        this.valueTask = valueTask;
+        this.createTask();
+    }
+    // function for CREATE TASK 
+    createTask() {
+        let arrayObjTasks;
+        if (localStorage.getItem('arrayObjTasks') != null) {
+            arrayObjTasks = localStorage.getItem('arrayObjTasks');
+        }
+        else if (localStorage.getItem('arrayObjTasks') === null) {
+            localStorage.setItem('arrayObjTasks', JSON.stringify([]));
+            arrayObjTasks = localStorage.getItem('arrayObjTasks');
+        }
+        // UPLOAD OBJ tASKS IN LOCAL STORAGE
+        Task.arrayObjTasks = JSON.parse(arrayObjTasks);
+        Task.arrayObjTasks.push({ id: this.id, value: this.valueTask });
+        localStorage.removeItem('arrayObjTasks');
+        localStorage.setItem('arrayObjTasks', JSON.stringify(Task.arrayObjTasks));
+        // Create ELEMENT
+        const list = document.querySelector('body .container .form .content .list');
+        const li = document.createElement('li');
+        li.className = `item`;
+        li.setAttribute('data-idTask', this.id);
+        li.innerHTML = `<a class="task">${this.valueTask}</a><a class="icon icon_edite btn btnEdite" title="Edite"><i class="fa-solid fa-pen"></i></a><a class="icon icon_delete btn btnDelete" title="Delete"><i class="fa-solid fa-trash"></i></a>`;
+        list.insertBefore(li, list.children[0]);
+        Task.btnDeleteTask(li.querySelector('.btnDelete'), this.id); //add event for button delete
+    }
+    // display Tasks
+    static displayTasks() {
+        let arrayObjTasks;
+        if (localStorage.getItem('arrayObjTasks') != null) {
+            arrayObjTasks = localStorage.getItem('arrayObjTasks');
+        }
+        Task.arrayObjTasks = JSON.parse(arrayObjTasks);
+        const list = document.querySelector('body .container .form .content .list');
+        for (let i = 0; i < Task.arrayObjTasks.length; i++) {
+            const li = document.createElement('li');
+            li.className = `item`;
+            li.setAttribute('data-idTask', Task.arrayObjTasks[i].id);
+            li.innerHTML = `<a class="task">${Task.arrayObjTasks[i].value}</a><a class="icon icon_edite btn btnEdite" title="Edite"><i class="fa-solid fa-pen"></i></a><a class="icon icon_delete btn btnDelete" title="Delete"><i class="fa-solid fa-trash"></i></a>`;
+            list.insertBefore(li, list.children[0]);
+            Task.btnDeleteTask(li.querySelector('.btnDelete'), Task.arrayObjTasks[i].id);
+        }
+        ;
+    }
+}
+Task.arrayTasksId = [];
+Task.idResult = '';
+Task.arrayObjTasks = [];
+// FUNCTION FOR SET Id to TASK
+Task.idTask = function (value) {
+    // create id for tasks
+    Task.idResult = `${value[0]}${value[value.length - 1]}`;
+    for (let i = 0; i < 7; i++) {
+        Task.idResult += `${Math.floor(Math.random() * 10)}`;
+    }
+    // check if include in array 'arrayTasksId' double id or same id for tasks
+    let bool = false;
+    const arrItems = localStorage.getItem('arrayTasksId') != null ? localStorage.getItem('arrayTasksId') : undefined;
+    if (typeof arrItems === 'string' && arrItems != undefined) {
+        Task.arrayTasksId = JSON.parse(arrItems);
+        for (let i = 0; i < Task.arrayTasksId.length; i++) {
+            if (Task.idResult === Task.arrayTasksId[i]) {
+                bool = true;
+                break;
+            }
+        }
+        bool === true ? Task.idTask(value) : false;
+    }
+    if (bool === false) {
+        Task.arrayTasksId.push(Task.idResult);
+        localStorage.removeItem('arrayTasksId');
+        localStorage.setItem('arrayTasksId', JSON.stringify(Task.arrayTasksId));
+    }
+    console.log(Task.idResult);
+    return Task.idResult;
+};
+// add event for button delete
+Task.btnDeleteTask = function (btnDelete, objId) {
+    btnDelete.addEventListener('click', function () {
+        console.log(btnDelete, objId);
+        // remove obj from localStorage 'arrayObjTasks'
+        const arrayObjTasks = localStorage.getItem('arrayObjTasks');
+        Task.arrayObjTasks = typeof arrayObjTasks != null ? JSON.parse(arrayObjTasks) : new Error('arrayObjTasks is null');
+        for (let i = 0; i < Task.arrayObjTasks.length; i++) {
+            if (Task.arrayObjTasks[i].id === objId) {
+                Task.arrayObjTasks.splice(i, 1);
+                localStorage.removeItem('arrayObjTasks');
+                localStorage.setItem('arrayObjTasks', JSON.stringify(Task.arrayObjTasks));
+                break;
+            }
+        }
+        // remove id obj from localStorage 'arrayTasksId'
+        const arrayTasksId = localStorage.getItem('arrayTasksId');
+        Task.arrayTasksId = typeof arrayTasksId != null ? JSON.parse(arrayTasksId) : new Error('arrayTasksId is null');
+        for (let i = 0; i < Task.arrayTasksId.length; i++) {
+            if (Task.arrayTasksId[i] === objId) {
+                Task.arrayTasksId.splice(i, 1);
+                localStorage.removeItem('arrayTasksId');
+                localStorage.setItem('arrayTasksId', JSON.stringify(Task.arrayTasksId));
+                break;
+            }
+        }
+        // remove element from list element
+        const list = document.querySelector('body .container .form .content .list');
+        for (let i = 0; i < list.childElementCount; i++) {
+            if (list.children[i].getAttribute('data-idTask') === objId) {
+                list.removeChild(list.children[i]);
+                break;
+            }
+        }
+    });
+};
+window.addEventListener('DOMContentLoaded', function () {
+    Task.displayTasks();
+    inpTask.value = ``;
+    inpTask.focus();
+});
